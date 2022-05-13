016298a31cde45f3874ea0d9b23284ec3864b2ac
Alicia Watt <alicia.watt.aw@gmail.com>
2022-05-13 12:36:38 -0600

    rubocop fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe User, type: :model do
     # it { should validate_uniqueness_of(:email).scoped_to(:id).with_message('** Email Already Exists **') }
 
     describe '.not_hosts' do
-    it "returns all the users but the passed user_id" do
-      user_1 = create(:user, name: 'Abby', email: 'abby@mail.org')
-      user_2 = create(:user, name: 'Bob', email: 'bob@mail.org')
-      user_3 = create(:user, name: 'Christy', email: 'christy@mail.org')
-      user_4 = create(:user, name: 'Dave', email: 'dave@mail.org')
+      it 'returns all the users but the passed user_id' do
+        user_1 = create(:user, name: 'Abby', email: 'abby@mail.org')
+        user_2 = create(:user, name: 'Bob', email: 'bob@mail.org')
+        user_3 = create(:user, name: 'Christy', email: 'christy@mail.org')
+        user_4 = create(:user, name: 'Dave', email: 'dave@mail.org')
 
-      expect(User.not_host(user_2.id)).to eq([user_1, user_3, user_4])
+        expect(User.not_host(user_2.id)).to eq([user_1, user_3, user_4])
+      end
     end
-  end
   end
 end
